83abf801c66f82cb4844983b5322f95a95a7128b
Endalebob <endaleyohannes8@gmail.com>
2022-01-14 16:18:16 +0300

    initialization

---
+++ agenagn/src/main/java/com/project/agenagn/AgenagnApplication.java
@@ -0,0 +1,13 @@
+package com.project.agenagn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenagnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenagnApplication.class, args);
+	}
+
+}
---
+++ agenagn/src/test/java/com/project/agenagn/AgenagnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.agenagn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenagnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
