31a62e286cf3ca9763d4f0c8e0ca7bdcb5c22705
kena <annatefera3@gmail.com>
2022-01-14 19:05:58 -0800

    home page

---
+++ project/src/main/java/com/agenagn/project/HomeController.java
@@ -0,0 +1,15 @@
+package com.agenagn.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+    
+}
