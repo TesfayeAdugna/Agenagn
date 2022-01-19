543cdd96963e4f4d7a50eadce350c9103ccc626a
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-01-19 16:28:25 +0300

    uploading photo

--- project/src/main/java/com/agenagn/project/HomeController.java
+++ project/src/main/java/com/agenagn/project/HomeController.java
@@ -1,21 +1,27 @@
 package com.agenagn.project;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @Autowired
     private ItemService service;
+    @Autowired
+    private ItemRepository repo;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -25,12 +31,20 @@ public class HomeController {
         return "home";
     }
     @GetMapping("/itemsform")
-    public String add(Model model){
+    public String sell(Model model){
         model.addAttribute("item", new Items());
         return "itemsform";
     }
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String saveItems(@ModelAttribute("item") Items items){
+    public String saveItems(@ModelAttribute("item") Items items, @RequestParam("image") MultipartFile multipartFile) throws IOException{
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        items.setPhotos(fileName);
+         
+        Items savedItems = repo.save(items);
+ 
+        String uploadDir = "user-photos/" + savedItems.getId();
+ 
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
         service.save(items);
         return "redirect:/";
     }
--- project/src/main/java/com/agenagn/project/Items.java
+++ project/src/main/java/com/agenagn/project/Items.java
@@ -1,5 +1,8 @@
 package com.agenagn.project;
 
+import java.beans.Transient;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,5 +22,13 @@ public class Items {
     private int price;
     private String contact;
     private String type;
+    @Column(nullable = true, length = 64)
     private String photos;
+
+    @Transient
+    public String getPhotosImagePath() {
+        if (photos == null || id == null) return null;
+         
+        return "/user-photos/" + id + "/" + photos;
+    }
 }
