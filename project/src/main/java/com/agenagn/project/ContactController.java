aaa0031e025cc388001be7b2677608ad53d1e2f4
Rediet-Ferew <91590944+Rediet-Ferew@users.noreply.github.com>
2022-01-20 08:25:53 -0900

    write some text here

---
+++ project/src/main/java/com/agenagn/project/ContactController.java
@@ -0,0 +1,17 @@
+package com.agenagn.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+        
+    @GetMapping("/contact")
+    public String contact(){
+    
+    
+    
+        return "contact";
+    }
+    
+}
