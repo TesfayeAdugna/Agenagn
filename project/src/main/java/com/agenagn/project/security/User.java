8e8033de8220c01882eb81eab087f86554d1c354
kena <annatefera3@gmail.com>
2022-01-23 19:17:54 +0300

    modifying admin page

--- project/src/main/java/com/agenagn/project/security/User.java
+++ project/src/main/java/com/agenagn/project/security/User.java
@@ -3,7 +3,6 @@ package com.agenagn.project.security;
 import java.util.Arrays;
 import java.util.Collection;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
